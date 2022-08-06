52fd14207b08abed9dc1ef0bde85788040fc7b0a
Roger Calaf Planell <rogercalaf@gmail.com>
2022-08-06 18:06:48 +0200

    Added toolbar component

--- UI.Angular.App/Expenses.App/src/app/app.module.ts
+++ UI.Angular.App/Expenses.App/src/app/app.module.ts
@@ -1,23 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatIconModule } from '@angular/material/icon'; 
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { ExpensesListComponent } from './expenses-list/expenses-list.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './footer/footer.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    ExpensesListComponent
+    ExpensesListComponent,
+    FooterComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    NoopAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
