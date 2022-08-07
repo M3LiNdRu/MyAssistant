59a60875a7896a6cc901f35ba144aa259ef3e349
Roger Calaf Planell <rogercalaf@gmail.com>
2022-08-07 20:28:30 +0200

    Hide button on click

--- UI.Angular.App/Expenses.App/src/app/float-button/float-button.component.ts
+++ UI.Angular.App/Expenses.App/src/app/float-button/float-button.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FloatButtonComponent implements OnInit {
 
+  isButtonVisible: boolean = true;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onClick(): void {
+    this.isButtonVisible = false;
   }
 
 }
