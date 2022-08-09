fa577fd7118e4a73c0f15a2109d6698203356ec0
Roger Calaf Planell <rogercalaf@gmail.com>
2022-08-09 20:57:50 +0200

    handling jwt auth

--- UI.Angular.App/Expenses.App/src/app/login/login.component.ts
+++ UI.Angular.App/Expenses.App/src/app/login/login.component.ts
@@ -1,16 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class LoginComponent implements AfterViewInit {
   
+  loggedIn: boolean = false;
+
+  constructor(private elementRef: ElementRef) {
+  }
+
+  ngOnInit() {
+    this.loggedIn = localStorage.getItem('id_token') != null;
+    (window as any).googleLogin = function(response: any) {
+      localStorage.setItem('id_token', response.credential);
+      this.loggedIn = true;
+    } 
+  }
+
+  ngAfterViewInit() {
+    const s = document.createElement("script");
+    s.type = "text/javascript";
+    s.src = "https://accounts.google.com/gsi/client";
+    this.elementRef.nativeElement.appendChild(s);
+  }
+
+  logout(): void {
+    localStorage.removeItem("id_token");
+    this.loggedIn = false;
+  }
 
 }
